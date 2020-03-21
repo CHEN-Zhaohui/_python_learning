b2c77ae387b59c851dcbe1a2c5257eaad5c5bfb6
CHEN-Zhaohui <czh_cea@163.com>
2020-03-22 00:22:40 +0800

    字符串Z变换

---
+++ convertZ.py
@@ -0,0 +1,19 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows < 2: return s
+        res = ["" for _ in range(numRows)]
+        i, flag = 0, -1
+        for c in s:
+            res[i] += c
+            if i == 0 or i == numRows - 1: flag = -flag
+            i += flag
+        print(res)
+        # join() 方法用于将序列中的元素以指定的字符连接生成一个新的字符串。
+        # str.join(sequence)
+        return "".join(res)
+
+
+if __name__ == '__main__':
+    test = Solution()
+    s = "LEETCODEISHIRING"
+    print(test.convert(s, 3))
